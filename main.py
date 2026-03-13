d4dc191cacf994e6c014cc768dd4d964cfde4f6c
alvizolc <boiialvizo@gmail.com>
2026-03-13 21:19:59 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#BASIC ARITHMETIC
+#friends = 5
+
+#friends += 2 #augmented assignment notation
+
+#print(friends)
+
+#TYPECASTING - the process of converting variable from one data type to another
+
+#name = "codeX"
+#age = 21
+#gpa = 2.15
+#is_student = True
+
+#name = bool(name)
+#print(name)
+
+
+
+var1 = float(input("Enter first digit: "))
+print(f"First digit: {var1}")
+var2 = float(input("Enter second digit: "))
+print(f"Second digit: {var2}")
+
+sum = var1 + var2
+float(sum)
+print(f"Sum: {sum}")
+
---
+++ variables.py
@@ -0,0 +1,16 @@
+# a variable is a usable container for value (strings, integers, floats, boolean)
+#variables behave as if it was the value that it contains
+
+full_name = "codeX"
+age = 21
+gpa = 2.15
+is_student = True
+
+print(f"Hey {full_name}")
+print(f"You are {age} years old.")
+print(f"Your GPA is {gpa}.")
+
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student")
